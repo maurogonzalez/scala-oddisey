9bcb7fc3eb8fde5f8ae580cd0dd03ba644522fd1
MGump <jmajma8@gmail.com>
2020-05-15 08:30:50 -0500

    Add mutable wrapper.

--- modules/fs2-kafka/src/main/scala/oddisey/KafkaStreamsClient.scala
+++ modules/fs2-kafka/src/main/scala/oddisey/KafkaStreamsClient.scala
@@ -1,13 +1,31 @@
 package oddisey
 
 import java.util.Properties
+import scala.concurrent.duration._
 
-import org.apache.kafka.common.serialization.Serde
-import org.apache.kafka.streams.kstream.Consumed
-import org.apache.kafka.streams.scala.StreamsBuilder
-import org.apache.kafka.streams.scala.kstream.KStream
-import org.apache.kafka.streams.{ KafkaStreams, StreamsConfig, Topology }
+import cats.effect.{IO, Timer}
+import cats.syntax.apply._
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig, Topology}
 
+
+class FKafkaStreams(streams: KafkaStreams)
+  extends WrapMutable[KafkaStreams](streams) {
+
+  def serve(implicit T: Timer[IO]): IO[Unit] =
+    (cleanUp *> start *> waitWhileIsUp).map(_ => println("Running")).handleErrorWith(_ => close)
+
+  def waitWhileIsUp(implicit T: Timer[IO]): IO[Unit] =
+      fs2.Stream.repeatEval(IO.sleep(500.millis) *> isUp).evalTap(r => IO(println(s"IsUp: $r" )))
+        .takeWhile(x => !x).compile.drain
+
+  def isUp: IO[Boolean] = status.map(s => s.isRunningOrRebalancing || s == KafkaStreams.State.CREATED)
+
+  def cleanUp: IO[Unit]              = execute(_.cleanUp())
+  def start: IO[Unit]                = execute(_.start)
+  def close: IO[Unit]                = execute(_.close)
+  def status: IO[KafkaStreams.State] = execute(_.state())
+
+}
 object KafkaStreamsClient {
 
   private def propsBuilder(host: String, port: Int, applicationId: String) = {
---
+++ modules/fs2-kafka/src/main/scala/oddisey/WrapMutable.scala
@@ -0,0 +1,11 @@
+package oddisey
+
+import cats.effect.IO
+import cats.syntax.flatMap._
+
+abstract class WrapMutable[A](private val a: A) {
+
+  final def execute[B](f: A => B): IO[B] = IO(f(a))
+  final def executeM[B](f: A => IO[B]): IO[B] = IO(f(a)).flatten
+
+}
