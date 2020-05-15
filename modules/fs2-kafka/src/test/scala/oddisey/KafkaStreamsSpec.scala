db7210d60fc362225b9326d875f0153c90f38ece
MGump <jmajma8@gmail.com>
2020-05-15 08:31:08 -0500

    Fix KStreams spec.

--- modules/fs2-kafka/src/test/scala/oddisey/KafkaStreamsSpec.scala
+++ modules/fs2-kafka/src/test/scala/oddisey/KafkaStreamsSpec.scala
@@ -1,25 +1,30 @@
 package oddisey
 
-import java.util.Date
+import java.util.UUID
+
 import scala.util.Try
 import scala.concurrent.duration._
 
-import cats.effect.{IO, Resource}
+import cats.effect.concurrent.Ref
+import cats.effect.IO
+import cats.instances.list._
 import cats.syntax.applicative._
 import cats.syntax.apply._
 import cats.syntax.flatMap._
+import fs2.concurrent.SignallingRef
 import fs2.kafka.{ProducerRecord, ProducerRecords, Serializer}
 import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.scala.Serdes._
 import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.common.serialization.Serde
 import org.apache.kafka.streams.kstream.Printed
+
 import oddisey.grpc.example.Odysseus
 
 class KafkaStreamsSpec extends KafkaBaseSpec {
-  val sourceTopic = "kafka-streams-1"
-  val sinktopic   = "kafka-streams-2"
-  val appId       = "kafka-streams-application-2"
+  val sourceTopic = "kafka-streams-source"
+  val sinktopic   = "kafka-streams-sink"
+  val appId       = "kafka-streams-application-source"
   val words       = "But be content with the food and drink aboard our ship ..."
 
   // Serdes
@@ -30,36 +35,48 @@ class KafkaStreamsSpec extends KafkaBaseSpec {
   implicit val streamSerde: Serde[Odysseus] = fromFn(Odysseus.toByteArray(_: Odysseus), odysseusParser)
 
   // KafkaStreams
-  def kStreamio(shoudThrow: Boolean) = IO {
+  def topology(shoudThrow: Boolean, ref: Ref[IO, List[Odysseus]]) = {
     val builder = new StreamsBuilder
     val kStream = builder.stream[String, Odysseus](sourceTopic).mapValues { t =>
-      if (shoudThrow) throw new Exception("Bum!")
-      t
+      val io =
+        if (shoudThrow) IO.raiseError(new Exception("Bum!"))
+        else ref.update(t :: _)
+
+      (IO.shift *> io).unsafeRunSync()
     }
-    kStream.to(sinktopic)
+//    kStream.to(sinktopic)
     kStream.print(Printed.toSysOut())
     builder
   }
 
   // Kafka Producer
   val producer = KafkaClient.kafkaProducer(host, port, keySerializer, odysseusSerializer)
-  val record   = ProducerRecord(sourceTopic, java.time.LocalDateTime.now().toString, Odysseus(words))
-  val produce  = producer.use(p => p.produce(ProducerRecords.one(record)).flatten)
+  def record   = ProducerRecord(sourceTopic, UUID.randomUUID().toString, Odysseus(UUID.randomUUID().toString))
+  val records  = (1 to 10).map(_ => record)
+  val produce  = producer.use(p => p.produce(ProducerRecords(records.toList)).flatten)
 
   test("kafkaStreams") {
-    def stream(shouldThrow: Boolean = false) = Resource.make(
-      kStreamio(shouldThrow).map(b => KafkaStreamsClient.stream(host, port, b.build(), appId))
-    )(s => IO(s.close()))
+    def stream(shouldThrow: Boolean, ref: Ref[IO, List[Odysseus]]) =
+      new FKafkaStreams(KafkaStreamsClient.stream(host, port, topology(shouldThrow, ref).build(), appId))
 
+    val spec = for {
+      ref    <- Ref.of[IO, List[Odysseus]](List())
+      signal <- SignallingRef[IO, Boolean](false)
+      stream1 = stream(false, ref)
+      _   <- produce
+      s1  <- stream1.serve.start
+      _ <- fs2.Stream
+            .repeatEval(ref.get)
+            .evalMap(r => signal.set(true).whenA(r.map(_.message).toSet == records.map(_.value.message).toSet))
+            .interruptWhen(signal)
+            .interruptAfter(5.seconds)
+            .compile
+            .drain
+      res <- ref.get
+      _   <- stream1.close *> s1.cancel
+    } yield assertEquals(res.map(_.message).toSet, records.map(_.value.message).toSet)
 
-    (for {
-     _  <- produce
-     s1 <- stream(true).use(s => IO(s.start()) *> IO.sleep(10.seconds)).start
-     s2 <- stream().use(s => IO(s.start()) *> IO.sleep(10.seconds)).start
-     _ <- s1.join
-     _ <- s2.join
-    } yield ())
-    .unsafeRunSync()
+    spec.unsafeRunSync()
 
   }
 
